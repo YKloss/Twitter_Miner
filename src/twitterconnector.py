4598f00df92ea09ec6d792fd05df21a259c4fdb3
Sven <sven.schirmer1@gmail.com>
2017-06-19 15:54:20 +0200

    Tweepy hinzugefügt

--- src/twitterconnector.py
+++ src/twitterconnector.py
@@ -1,9 +1,11 @@
-class TwitterConnector():
+import tweepy
 
-    def __init__(self):
-        pass
+class TwitterConnector():
+    def __init__(self, consumer_key, consumer_secret):   
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.api = tweepy.API(auth)
 
     def get_tweets(self, hastag):
-        tweets = ["tweet1", "tweet2"]
+        tweets = tweepy.Cursor(self.api.search, q=hastag).items()
         #return complete tweet, not only the text
         return tweets

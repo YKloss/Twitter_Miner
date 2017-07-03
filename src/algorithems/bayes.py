4baf9c27dc95afe3d9af7eccfe6977aad6da7809
Sven <sven.schirmer1@gmail.com>
2017-07-03 17:19:13 +0200

    clf Name angepasst

--- src/algorithems/bayes.py
+++ src/algorithems/bayes.py
@@ -15,7 +15,7 @@ class NaiveBayes(AbstractClassifier):
     def train(self, texts, classes):
         self.logger.debug("Start training...")
         model = Pipeline([('vect', TfidfVectorizer(max_df=0.95, min_df=2, max_features=6000, decode_error='ignore')),
-                          ('bayeszz', MultinomialNB()),
+                          ('clf', MultinomialNB()),
                           ])
         #model = Pipeline([('vect', TfidfVectorizer()),
         #                  ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)),
